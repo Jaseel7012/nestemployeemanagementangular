3cf02672f67a106c473a9bbe92d01d41122ca42d
jaseel <jaseeljctcs@gmail.com>
2022-12-13 14:08:22 +0530

    initial

---
+++ src/app/add-employee/add-employee.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { NewApiService } from '../new-api.service';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent {
+name=""
+designation=""
+username=""
+email=""
+passwo=""
+constructor(private api:NewApiService){}
+read=()=>{
+ let data:any={
+  "name":this.name,
+    "designation":this.designation,
+    "username":this.username,
+    "passwo": this.passwo
+
+ }
+ 
+ 
+  
+  
+
+
+
+
+}}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,46 @@ import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { LoginEmployeeComponent } from './login-employee/login-employee.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { SearchEmpComponent } from './search-emp/search-emp.component';
+import { EditEmpComponent } from './edit-emp/edit-emp.component';
+import {HttpClientModule} from '@angular/common/http'
 const myroute:Routes=[
 {
   path:''
   ,component:AdminComponent
+},{
+  path:'addemp',
+  component:AddEmployeeComponent
+},{
+  path:'loginemp',
+  component:LoginEmployeeComponent
+},{
+  path:'empsearch',
+  component:SearchEmpComponent
+},{
+  path:'edit',
+  component:EditEmpComponent
 }
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    AddEmployeeComponent,
+    LoginEmployeeComponent,
+    ViewEmployeeComponent,
+    SearchEmpComponent,
+    EditEmpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(myroute)
+    RouterModule.forRoot(myroute),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
